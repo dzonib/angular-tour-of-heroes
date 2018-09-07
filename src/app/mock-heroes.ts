82f36ecddfe4e3851282467f1554d863cd5a57e2
dzonib <nikolabosnjak381@gmail.com>
2018-09-07 13:58:14 +0200

    heroes all around

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Death'},
+  {id: 12, name: 'Life'},
+  {id: 13, name: 'Omega'},
+  {id: 14, name: 'Alpha'},
+  {id: 15, name: 'King'},
+  {id: 16, name: 'Fuckin'},
+  {id: 17, name: 'Kong'},
+  {id: 18, name: 'Whatup'},
+  {id: 19, name: 'Bruh'},
+];
